be039a5c33276ea896871fd00978ebeeac6911a8
Mamannne <aymane@hamdaoui.eu>
2024-02-29 23:00:14 +0100

    défi 7 et Test 7

--- out/app.js
+++ out/app.js
@@ -3,6 +3,7 @@ document.addEventListener('keydown', haut);
 document.addEventListener('keydown', bas);
 document.addEventListener('keydown', droit);
 document.addEventListener('keydown', gauche);
+document.addEventListener('keydown', change_score);
 function bonjour() {
     const message_01 = "Bonjour";
     console.log(message_01);
@@ -30,4 +31,26 @@ function gauche(event) {
         console.log('La flèche de gauche a été enfoncée');
     }
 }
+function change_style_score() {
+    const mon_score = document.getElementById('score');
+    if (mon_score) {
+        mon_score.style.textAlign = 'center';
+        mon_score.style.color = 'purple';
+        mon_score.style.backgroundColor = 'grey';
+        mon_score.style.fontSize = '50px';
+    }
+    else {
+        console.error('Pas de score');
+    }
+}
+function change_score(event) {
+    const mon_score = document.getElementById('score');
+    if (event.key == 'ArrowLeft' || event.key == 'ArrowRight'
+        || event.key == 'ArrowDown' || event.key == 'ArrowUp') {
+        let score = parseInt(mon_score.textContent);
+        score++;
+        mon_score.textContent = score.toString();
+    }
+}
+change_style_score();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ document.addEventListener('keydown', haut);
 document.addEventListener('keydown', bas);
 document.addEventListener('keydown', droit);
 document.addEventListener('keydown', gauche);
+document.addEventListener('keydown', change_score);
 
 
 function bonjour(): void{
@@ -42,3 +43,32 @@ function gauche(event: KeyboardEvent): void{
     }
 
 }
+
+function change_style_score(): void{
+    const mon_score = document.getElementById('score')
+
+    if (mon_score){
+        mon_score.style.textAlign = 'center';
+        mon_score.style.color = 'purple'; 
+        mon_score.style.backgroundColor = 'grey'; 
+        mon_score.style.fontSize = '50px';
+    }
+
+    else {
+        console.error('Pas de score')
+    }
+}
+
+function change_score(event: KeyboardEvent): void {
+
+    const mon_score = document.getElementById('score');
+
+    if (event.key == 'ArrowLeft' || event.key == 'ArrowRight' 
+    || event.key == 'ArrowDown' || event.key == 'ArrowUp'){
+        let score = parseInt(mon_score.textContent);
+        score++;
+        mon_score.textContent = score.toString();
+    }
+}
+
+change_style_score()
