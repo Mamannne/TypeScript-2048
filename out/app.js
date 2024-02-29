030e973f935ea4ce058000b3a07405c513674e96
Mamannne <aymane@hamdaoui.eu>
2024-02-29 22:06:24 +0100

    défi 4

---
+++ out/app.js
@@ -0,0 +1,9 @@
+testbonjour();
+function bonjour() {
+    const message_01 = "Bonjour!";
+    console.log(message_01);
+}
+function testbonjour() {
+    bonjour();
+}
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+
+testbonjour();
+
+function bonjour(): void{
+    const message_01: string = "Bonjour";
+    console.log(message_01);
+}
+
+function testbonjour(): void{
+    bonjour();
+}
