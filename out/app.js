040c523a773dc19f687e15243670a531c7e36b16
Mamannne <aymane@hamdaoui.eu>
2024-02-29 22:36:47 +0100

    Défi 6

--- out/app.js
+++ out/app.js
@@ -1,9 +1,33 @@
 testbonjour();
+document.addEventListener('keydown', haut);
+document.addEventListener('keydown', bas);
+document.addEventListener('keydown', droit);
+document.addEventListener('keydown', gauche);
 function bonjour() {
-    const message_01 = "Bonjour!";
+    const message_01 = "Bonjour";
     console.log(message_01);
 }
 function testbonjour() {
     bonjour();
+}
+function haut(event) {
+    if (event.key == 'ArrowUp') {
+        console.log('La flèche du haut a été enfoncée');
+    }
+}
+function bas(event) {
+    if (event.key == 'ArrowDown') {
+        console.log('La flèche du bas a été enfoncée');
+    }
+}
+function droit(event) {
+    if (event.key == 'ArrowRight') {
+        console.log('La flèche de droite a été enfoncée');
+    }
+}
+function gauche(event) {
+    if (event.key == 'ArrowLeft') {
+        console.log('La flèche de gauche a été enfoncée');
+    }
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 
 testbonjour();
+document.addEventListener('keydown', haut);
+document.addEventListener('keydown', bas);
+document.addEventListener('keydown', droit);
+document.addEventListener('keydown', gauche);
+
 
 function bonjour(): void{
     const message_01: string = "Bonjour";
@@ -8,4 +13,32 @@ function bonjour(): void{
 
 function testbonjour(): void{
     bonjour();
+}
+
+function haut(event: KeyboardEvent): void{
+    if (event.key == 'ArrowUp'){
+        console.log('La flèche du haut a été enfoncée');
+    }
+
+}
+
+function bas(event: KeyboardEvent): void{
+    if (event.key == 'ArrowDown'){
+        console.log('La flèche du bas a été enfoncée');
+    }
+
+}
+
+function droit(event: KeyboardEvent): void{
+    if (event.key == 'ArrowRight'){
+        console.log('La flèche de droite a été enfoncée');
+    }
+
+}
+
+function gauche(event: KeyboardEvent): void{
+    if (event.key == 'ArrowLeft'){
+        console.log('La flèche de gauche a été enfoncée');
+    }
+
 }
